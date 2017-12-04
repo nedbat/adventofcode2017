ccf9fbd78d638f72d9df00bb9e2e2bcbcc19363e
Ned Batchelder <ned@nedbatchelder.com>
2017-12-04 05:35:49 -0500

    Day 4, part 2

--- day04.py
+++ day04.py
@@ -5,8 +5,8 @@ http://adventofcode.com/2017/day/4
 import pytest
 
 
-def is_valid(passphrase):
-    words = passphrase.split()
+def is_valid(phrase):
+    words = phrase.split()
     unique_words = set(words)
     return len(unique_words) == len(words)
 
@@ -24,3 +24,27 @@ if __name__ == '__main__':
         num_valid = sum(int(is_valid(line)) for line in finput)
 
     print(f"Part 1: there are {num_valid} valid passphrases")
+
+
+def is_valid2(phrase):
+    words = phrase.split()
+    canon_words = ["".join(sorted(w)) for w in words]
+    unique = set(canon_words)
+    return len(unique) == len(canon_words)
+
+@pytest.mark.parametrize("phrase, valid", [
+    ("abcde fghij", True),
+    ("abcde xyz ecdab", False),
+    ("a ab abc abd abf abj", True),
+    ("iiii oiii ooii oooi oooo", True),
+    ("oiii ioii iioi iiio", False),
+])
+def test_is_valid2(phrase, valid):
+    assert is_valid2(phrase) == valid
+
+
+if __name__ == '__main__':
+    with open("day04_input.txt") as finput:
+        num_valid = sum(int(is_valid2(line)) for line in finput)
+
+    print(f"Part 2: there are {num_valid} valid passphrases")
