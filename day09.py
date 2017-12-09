7c2c380070d1f78dbfee276d62446a71a29a243f
Ned Batchelder <ned@nedbatchelder.com>
2017-12-09 06:24:44 -0500

    Day 9, part 2

--- day09.py
+++ day09.py
@@ -14,14 +14,18 @@ def unbanged_chars(stream):
         else:
             yield ch
 
-def score(stream):
+def process(stream):
+    """Returns total_score, garbage_chars."""
     total = 0
     depth = 0
     garbage = False
+    gchars = 0
     for ch in unbanged_chars(stream):
         if garbage:
             if ch == '>':
                 garbage = False
+            else:
+                gchars += 1
         else:
             if ch == '{':
                 depth += 1
@@ -30,7 +34,8 @@ def score(stream):
                 depth -= 1
             elif ch == '<':
                 garbage = True
-    return total
+    return total, gchars
+
 
 @pytest.mark.parametrize("stream, answer", [
     ("{}", 1),
@@ -43,10 +48,25 @@ def score(stream):
     ("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3),
 ])
 def test_score(stream, answer):
-    assert score(stream) == answer
+    assert process(stream)[0] == answer
+
+
+@pytest.mark.parametrize("stream, answer", [
+    ('<>', 0),
+    ('<random characters>', 17),
+    ('<<<<>', 3),
+    ('<{!>}>', 2),
+    ('<!!>', 0),
+    ('<!!!>>', 0),
+    ('<{o"i!a,<{i<a>', 10),
+])
+def test_garbage_chars(stream, answer):
+    assert process(stream)[1] == answer
 
 
 if __name__ == '__main__':
     with open("day09_input.txt") as finput:
         stream = finput.read()
-    print(f"Part 1: total score is {score(stream)}")
+    score, gchars = process(stream)
+    print(f"Part 1: total score is {score}")
+    print(f"Part 1: there are {gchars} non-canceled characters within the garbage")
