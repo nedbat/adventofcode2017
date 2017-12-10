6c351e447d6a8feede44202d55b5004070e5185e
Ned Batchelder <ned@nedbatchelder.com>
2017-12-10 09:38:49 -0500

    Day 10, part 2

--- day10.py
+++ day10.py
@@ -1,3 +1,10 @@
+"""
+http://adventofcode.com/2017/day/10
+"""
+
+import functools
+import operator
+
 import pytest
 
 
@@ -50,14 +57,15 @@ def test_set(start, stop, length, result):
     assert lst == result
 
 
-def knot_hash(data, lengths):
+def knot_hash(data, lengths, rounds=1):
     circle = Circle(data)
     pos = 0
     skip = 0
-    for length in lengths:
-        circle[pos:pos+length] = reversed(circle[pos:pos+length])
-        pos += length + skip
-        skip += 1
+    for _ in range(rounds):
+        for length in lengths:
+            circle[pos:pos+length] = reversed(circle[pos:pos+length])
+            pos += length + skip
+            skip += 1
 
 
 def test_knot_hash():
@@ -65,10 +73,46 @@ def test_knot_hash():
     knot_hash(data, [3, 4, 1, 5])
     assert data == [3, 4, 2, 1, 0]
 
+INPUT = "97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190"
 
 if __name__ == '__main__':
-    INPUT = list(map(int, "97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190".split(",")))
+    lengths = list(map(int, INPUT.split(",")))
     data = list(range(256))
-    knot_hash(data, INPUT)
+    knot_hash(data, lengths)
     result = data[0] * data[1]
     print(f"Part 1: multiplying the first two numbers gives {result}")
+
+
+def ascii_codes(s):
+    return map(ord, s)
+
+def test_ascii_codes():
+    assert list(ascii_codes("1,2,3")) == [49,44,50,44,51]
+
+STD_LENGTH_SUFFIX = [17, 31, 73, 47, 23]
+
+
+def full_knot_hash(data):
+    lengths = list(ascii_codes(data)) + STD_LENGTH_SUFFIX
+    data = list(range(256))
+    knot_hash(data, lengths, rounds=64)
+
+    hexbytes = []
+    for i in range(0, 256, 16):
+        xored = functools.reduce(operator.xor, data[i:i+16])
+        hexbytes.append("{:02x}".format(xored))
+
+    return "".join(hexbytes)
+
+@pytest.mark.parametrize("data, hexhash", [
+    ("", "a2582a3a0e66e6e86e3812dcb672a272"),
+    ("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd"),
+    ("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d"),
+    ("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e"),
+])
+def test_full_knot_hash(data, hexhash):
+    assert full_knot_hash(data) == hexhash
+
+
+if __name__ == '__main__':
+    print(f"Part 2: the knot hash is {full_knot_hash(INPUT)}")
