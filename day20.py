d1a7d52bc055b8e74978bbcdac5c0feb2b0742bd
Ned Batchelder <ned@nedbatchelder.com>
2017-12-20 09:12:49 -0500

    Day 20, part 2

--- day20.py
+++ day20.py
@@ -2,6 +2,7 @@
 http://adventofcode.com/2017/day/20
 """
 
+import itertools
 import re
 
 class Xyz:
@@ -12,6 +13,9 @@ class Xyz:
 
     def __repr__(self):
         return f"<{self.x},{self.y},{self.z}>"
+
+    def __eq__(self, other):
+        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
 
     def __add__(self, other):
         return Xyz(self.x + other.x, self.y + other.y, self.z + other.z)
@@ -44,8 +48,46 @@ class Pt:
 def read_points(lines):
     return [Pt.parse(i, line) for i, line in enumerate(lines)]
 
+def eventual_distance_key(pt):
+    """The key to use when determining the long-term distance from 0."""
+    return (len(pt.a), len(pt.v), len(pt.p))
+
+def distance_key(pt):
+    """The key to use when determining the current distance from 0."""
+    return len(pt.p), pt.p.x, pt.p.y, pt.p.z
+
 if __name__ == '__main__':
     with open("day20_input.txt") as finput:
         points = read_points(finput)
-    closest = min(points, key=lambda pt: (len(pt.a), len(pt.v), len(pt.p)))
+    closest = min(points, key=eventual_distance_key)
     print(f"Part 1: the point that stays closest to 0 is {closest}")
+
+def in_order(points):
+    """Are the points all in the order they will remain forever?"""
+    return sorted(points, key=eventual_distance_key) == sorted(points, key=distance_key)
+
+def remove_dups(seq, key):
+    for _, g in itertools.groupby(seq, key=key):
+        g = list(g)
+        if len(g) == 1:
+            yield g[0]
+
+
+def run_collisions(points):
+    steps = 0
+    while not in_order(points):
+        points.sort(key=distance_key)
+        new_points = list(remove_dups(points, key=lambda pt: pt.p))
+        if len(points) != len(new_points) or steps % 100 == 0:
+            print(f"After {steps} steps, {len(new_points)} points")
+        points = new_points
+        for pt in points:
+            pt.update()
+        steps += 1
+    return points
+
+if __name__ == '__main__':
+    with open("day20_input.txt") as finput:
+        points = read_points(finput)
+    points = run_collisions(points)
+    print(f"Part 2: {len(points)} particles are left")
